1b16910d518ee2052fe234cc5448643c3b60fbe9
Colin <cstatro@gmail.com>
2019-06-28 22:19:03 -0400

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,30 @@
 class Gym
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    Gym.all << self 
+  end
+  def self.all 
+    @@all
+  end
+  def recruit_member(cost,member)
+    Membership.new(cost,member,self)
+  end
+  def memberships 
+    Membership.all.select {|m| m.gym == self}
+  end
+  def all_lifters 
+    Membership.all.select {|m| m.gym == self}
+    .map {|m| m.member}
+  end
+  def all_names
+    Membership.all.select {|m| m.gym == self}
+    .map {|m| m.member.name}
+  end
+  def lift_total
+    Membership.all.select {|m| m.gym == self}
+    .map {|m| m.member.lift_total}.reduce(:+)
   end
 end
+
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,32 @@
 class Lifter
+  attr_accessor :memberships
   attr_reader :name, :lift_total
-
+  @@all  = []
   def initialize(name, lift_total)
     @name = name
+    @memberships
     @lift_total = lift_total
+    Lifter.all << self
+  end
+  def self.all 
+    @@all 
+  end 
+  def sign_up cost,gym 
+    Membership.new(cost,self,gym)
+  end
+  def all_memberships 
+    Membership.all.select {|m| m.member == self}
+  end
+  def all_gyms 
+    Membership.all.select {|m| m.member == self}
+    .map {|m| m.gym}
+  end
+  def self.average_lift
+    Membership.all.map {|m| m.member.lift_total}
+    .reduce(:+)/Membership.all.length 
+  end
+  def cost 
+    Membership.all.select {|m| m.member == self}
+    .map {|m| m.cost}.reduce(:+)
   end
 end
