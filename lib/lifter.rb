759ac352e41ed516e88a1ddd135e66fa89bfb5d3
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-29 22:05:35 -0400

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,42 @@
 class Lifter
-  attr_reader :name, :lift_total
+  attr_reader :name, :lift_total, :gyms, :memberships
+
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @memberships = []
+    @gyms = []
+    @@all << self
+  end
+
+  def join_gym(gym, memCost)
+    #sign up for gym
+    newMem = Membership.new(memCost)
+    gym.memberships << newMem
+    self.memberships << newMem
+    gym.lifters << self
+    self.gyms << gym
+  end
+
+  def total_cost
+    total = 0
+    self.memberships.each { |membership| total += membership.cost }
+    total
+  end
+
+  def self.average_lift
+    total = 0.0
+    Lifter.all.each { |lifter| total += lifter.lift_total }
+    return total / Lifter.count_lifters
+  end
+
+  def self.count_lifters
+    Lifter.all.count
+  end
+
+  def self.all
+    @@all
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,26 @@
 # You don't need to require any of the files in lib or pry.
 # We've done it for you here.
-require_relative '../config/environment.rb'
+require_relative "../config/environment.rb"
 
 # test code goes here
+
+# ben = Lifter.new("Ben", 100)
+# yos = Lifter.new("Yos", 150)
+# yos1 = Lifter.new("Yos1", 150)
+# yos2 = Lifter.new("Yos2", 150)
+# yos3 = Lifter.new("Yos3", 150)
+# yos4 = Lifter.new("Yos4", 150)
+
+# jimmy1 = Gym.new("Jimmy1")
+# jimmy2 = Gym.new("Jimmy2")
+# jimmy3 = Gym.new("Jimmy3")
+# jimmy4 = Gym.new("Jimmy4")
+
+# ben.join_gym(jimmy1, 45)
+# yos.join_gym(jimmy1, 45)
+# yos.join_gym(jimmy3, 79)
+# ben.join_gym(jimmy2, 45)
+# ben.join_gym(jimmy3, 79)
 
 binding.pry
 
