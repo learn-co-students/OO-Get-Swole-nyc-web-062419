8c5813d28fae6173663fd5785962b663abca6dfa
Flatiron School <email.the.anderson@gmail.com>
2019-07-01 09:03:07 -0400

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,23 @@
 class Gym
   attr_reader :name
+  attr_accessor :memberships, :lifters, :lifter_names, :group_lift_total
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @memberships = []
+    @lifters = []
+    @lifter_names = []
+    @group_lift_total = 0
   end
+
+  def gym_lift_total
+    group_lift_total / memberships.length
+  end
+
+  def self.all
+    @@all
+  end 
+
 end
