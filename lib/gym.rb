8e7c5fa398ba2c56895a661bc4c495e9ed686355
KConlon13 <KyleConlon13@gmail.com>
2019-06-30 20:27:28 -0400

    finished woo

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,39 @@
 class Gym
+  attr_accessor :lifters, :memberships
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
-end
+
+  def memberships
+    Membership.all.select do |member_instance|
+      member_instance.gym == self
+    end
+  end
+
+  def lifters
+    self.memberships.collect do |member_instance|
+      member_instance.lifter
+    end
+  end
+
+  def gym_lifters
+    self.memberships.collect do |member_instance|
+      member_instance.lifter
+    end
+  end
+
+  def gym_lifters_lift_totals
+    self.lifters.collect do |lifters|
+      lifters.lift_total
+    end.sum
+  end
+
+  def self.all
+    @@all
+  end
+
+end 
--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,18 @@
 class Membership
+  attr_accessor :gym, :lifter
   attr_reader :cost
+  @@all = []
 
-  def initialize(cost)
+  def initialize(cost, lifter, gym)
     @cost = cost
+    @lifter = lifter
+    @gym = gym
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
 end
+ 
