d5c4fc90ee531c1a437cbc9a25867f7d97bc8108
nicholasstano <nicholasstano@gmail.com>
2019-07-01 09:03:59 -0400

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,48 @@
+require 'pry'
+
 class Gym
+  
   attr_reader :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    Gym.all << self
   end
+
+  def self.all 
+  	# Get a list of all gyms
+  	@@all 
+  end 
+
+  def memberships
+  	# Get a list of all memberships at a specific gym
+  	Membership.all.select do |membership_instance|
+  		membership_instance.gym == self 
+  	end
+  end
+
+   def lifters
+  	# Get a list of all the lifters that have a membership to a specific gym
+  	memberships.map do |membership|
+  		membership.lifter
+  	end
+  end
+
+  def lifter_name
+  	# Get a list of the names of all lifters that have a membership to that gym
+  	lifters.map do |lifter|
+  		lifter.name
+  	end
+  end
+
+  def combined_lift_total 
+  	# Get the combined lift total of every lifter has a membership to that gym
+  	lifters.map do |membership|
+  		membership.lift_total
+  	end.inject(0.0) {|sum, element| sum + element}
+  end
+
 end
+
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,33 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+awl = Lifter.new("Awl", 444)
+bar = Lifter.new("Bar", 475)
+cru = Lifter.new("Cru", 410)
+dym = Lifter.new("Dym", 332)
+ele = Lifter.new("Ele", 290)
+frk = Lifter.new("Frk", 777)
+jon = Lifter.new("Jon", 512)
+mat = Lifter.new("Mat", 555)
+tre = Lifter.new("Tre", 666)
+war = Lifter.new("War", 500)
+
+gym1 = Gym.new("Gym 1")
+gym2 = Gym.new("Gym 2")
+gym3 = Gym.new("Gym 3")
+gym4 = Gym.new("Gym 4")
+
+mem1 = Membership.new(war, gym1, 50)
+mem2 = Membership.new(war, gym2, 75)
+mem3 = Membership.new(war, gym3, 100)
+mem4 = Membership.new(war, gym4, 125)
+mem5 = Membership.new(frk, gym1, 50)
+mem6 = Membership.new(jon, gym2, 75)
+mem7 = Membership.new(ele, gym3, 100)
+mem8 = Membership.new(cru, gym4, 125)
+
+
+
 binding.pry
 
 puts "Gains!"
